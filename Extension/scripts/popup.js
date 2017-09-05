6c7898dab92afbce2dc244883fb3f40d5b9e170e
Jack <devjacksmith@gmail.com>
2017-09-05 17:44:24 -0600

    Improved options to work with Firefox

--- Extension/scripts/popup.js
+++ Extension/scripts/popup.js
@@ -31,3 +31,13 @@ function displayErrorPopup(message) {
         error_popup.style.display = 'none';
     }, 2000);
 }
+
+document.getElementById('options').addEventListener('click', function() {
+    if (chrome.runtime.openOptionsPage) {
+        // New way to open options pages, if supported (Chrome 42+).
+        chrome.runtime.openOptionsPage();
+    } else {
+        // Reasonable fallback.
+        window.open(chrome.runtime.getURL('options.html'));
+    }
+});
