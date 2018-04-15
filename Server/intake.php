faa9b7d4a0b9ffba35328149d78a795aaae3eb99
Jack <devjacksmith@gmail.com>
2018-04-15 01:05:26 +0000

    Added giveaway integration

--- Server/intake.php
+++ Server/intake.php
@@ -232,6 +232,11 @@ if (!empty($_POST['hunt_details']) && !empty($hunt_id)) {
     }
 }
 
+// Giveaway
+if ($_POST['entry_timestamp'] >= $giveaway_start_time && $_POST['entry_timestamp'] <= $giveaway_end_time) {
+	recordGiveawayHunt($_POST['user_id'], $_POST['entry_timestamp']);
+}
+
 sendResponse('success', "Thanks for the hunt info!");
 
 function sendResponse($status, $message) {
@@ -240,6 +245,14 @@ function sendResponse($status, $message) {
         'message' => $message
     ]);
     die($response);
+}
+
+function recordGiveawayHunt($user = '', $timestamp = '') {
+	global $giveaway_url, $giveaway_key;
+	$url = $giveaway_url;
+	$url .= '?user=' . $user . '&ts=' . $timestamp;
+	$url .= '&mykey=' . $giveaway_key;
+	$nothing = file_get_contents($url);
 }
 
 function formatVersion($version) {
