1c6532477a44eef4db20fe8064d653b6e0bcfd5c
Jack <devjacksmith@gmail.com>
2018-11-06 22:43:17 +0000

    disabled missing mice part from tracker

--- Server/tracker.php
+++ Server/tracker.php
@@ -51,7 +51,7 @@
         </h3><br/><br/>
 
         <?php require_once "stats.php"; ?><br/><br/>
-        <?php require_once "missing_mice.php"; ?>
+        <?php //require_once "missing_mice.php"; ?>
 
         <br/><p class="text-center">For more info, copy of the data, or if you want to help with data gathering, please look <a href="https://www.agiletravels.com">here</a>.</p>
     </div>
