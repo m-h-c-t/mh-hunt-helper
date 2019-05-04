e19f7e9c14b23a01885552437d19fe2198f2f753
Jack <devjacksmith@gmail.com>
2019-05-04 21:16:42 +0000

    Updated backup config

--- DB/mh_db_auto_backup_nightly.sh
+++ DB/mh_db_auto_backup_nightly.sh
@@ -2,9 +2,8 @@
 echo "===== started mh_db_auto_backup_nightly.sh ======"
 
 source /var/www/mh-hunt-helper/DB/config.sh
-
 cd /keybase/public/devjacksmith/mh_backups/nightly
-date > last_updated.txt
+
 # Hunt Helper
 echo "====== Backing up hunt helper ====="
 
@@ -18,10 +17,13 @@ fi
 
 mysqldump -u $MH_USER -p$MH_PASS --host=127.0.0.1 --skip-lock-tables --events --routines mhhunthelper | gzip -9 > hunthelper_nightly.sql.gz
 sleep 5s
-rm -rf /var/lib/mysql-files/*
-mysqldump -u $MH_USER -p$MH_PASS --host=127.0.0.1 --skip-lock-tables --events --routines -T /var/lib/mysql-files/ --no-create-info --compatible=db2 mhhunthelper
-rm -rf /var/lib/mysql-files/*.sql
-zip -j -9 hunthelper_nightly.txt.zip /var/lib/mysql-files/*
-rm -rf /var/lib/mysql-files/*
+rm -rf $MH_DUMP/*
+mysqldump -u $MH_USER -p$MH_PASS --host=127.0.0.1 --skip-lock-tables --events --routines -T $MH_DUMP --no-create-info --compatible=db2 mhhunthelper
+rm -rf $MH_DUMP/*.sql
+zip -j -9 hunthelper_nightly.txt.zip $MH_DUMP/*
+rm -rf $MH_DUMP/*
 
 echo "===== finished mh_db_auto_backup_nightly.sh ====="
+
+date > last_updated.txt
+
