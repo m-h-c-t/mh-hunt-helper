d8c6590e1dadd30b30fbba1e9ac1032d0826a2ae
Rob Wilson <aardwolf98@gmail.com>
2020-06-22 20:22:01 +0000

    Renamed min/max column

--- Server/scripts/loot.js
+++ Server/scripts/loot.js
@@ -87,9 +87,9 @@ $( function() {
         var show_rate_per_catch = $('#rate_per_catch').is(':checked');
         var rate_per_catch_title = '';
         if (show_rate_per_catch) {
-            rate_per_catch_title = '<th>Rate per catch</th><th>Catches</th><th>Drop%</th><th>Min / Max</th>';
+            rate_per_catch_title = '<th>Qty/Catch</th><th>Catches</th><th>Drop%</th><th>Drop Qty Range</th>';
         }
-        var final_html = '<table id="results_table" class="table table-striped table-hover" style="width:100%"><thead><tr><th>Location</th><th>Stage</th><th>Cheese</th><th>Rate per hunt</th><th>Hunts</th>' + rate_per_catch_title + '</tr></thead><tbody>';
+        var final_html = '<table id="results_table" class="table table-striped table-hover" style="width:100%"><thead><tr><th>Location</th><th>Stage</th><th>Cheese</th><th>Qty/Hunt</th><th>Hunts</th>' + rate_per_catch_title + '</tr></thead><tbody>';
 
         var all_stages = '';
         data.forEach(function(row) {
@@ -99,10 +99,10 @@ $( function() {
                 + row.location + '</td><td>'
                 + stage + '</td><td>'
                 + row.cheese + '</td><td>'
-                + parseFloat(((row.rate)/1000).toFixed(3)) + '</td><td>'
+                + parseFloat(row.total_drops / row.total_hunts).toPrecision(5) + '</td><td>'
                 + row.total_hunts + '</td>';
             if (show_rate_per_catch) {
-                final_html += '<td>' + parseFloat(row.total_drops / row.total_catches).toFixed(5) + '</td><td>' + row.total_catches + '</td>' 
+                final_html += '<td>' + parseFloat(row.total_drops / row.total_catches).toPrecision(5) + '</td><td>' + row.total_catches + '</td>' 
                 	+ '<td>' + parseFloat(row.drop_pct).toFixed(2) + '%</td><td>' + row.min_amt + ' / ' + row.max_amt + '</td>';
             }
             final_html += '</tr>';
