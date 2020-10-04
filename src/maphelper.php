54d162469408c788e5ed14db7293f9559bb068a7
Jack <devjacksmith@gmail.com>
2020-10-04 19:30:36 +0100

    Fixing map helper redirect

--- src/maphelper.php
+++ src/maphelper.php
@@ -1,23 +1,6 @@
-<html lang="en">
-<head>
-<meta charset="utf-8">
-    <base href="/">
-    <?php require "common_head.php"; ?>
-
-
-</head>
-<body style="text-align: center;" class="text-center">
-    <div id="loader" class="loader"></div>
-    <script>$("#loader").css( "display", "block" );</script>
-    <div class="jumbotron">
-        <h1>MHCT User History</h1>
-        <a href="/" class="clickable"><span class="glyphicon glyphicon-chevron-left"></span> MHCT Tools</a>
-    </div>
-    <div class="container-fluid">
 <?php
 
-?>
-</div>
-<?php require "common_footer.php"; ?>
-</body>
-</html>
+header( "refresh:2;url=https://tsitu.github.io/MH-Tools/map.html" );
+
+echo "Under Maintenance... Redirecting to Tsitu's for now: <a href='https://tsitu.github.io/MH-Tools/map.html'>https://tsitu.github.io/MH-Tools/map.html</a>";
+
