60b590d10f3d618a7008432fe3d46412596a1af0
owentheman <imsmartypants@gmail.com>
2020-10-07 01:23:10 +0800

    removed attractions.php from repo and removed attraction rate section from index.php

--- src/index.php
+++ src/index.php
@@ -27,11 +27,6 @@
                 </td></tr>
             <tr><td>
                     <a tabindex="0" class="glyphicon glyphicon-question-sign pull-right popover_styles" role="button" data-toggle="popover"
-                       data-content="Search by mouse name to see the location, cheese and attraction rate of a specific mouse."></a>
-                    <a href="/attractions.php" style="display:block;text-decoration:none;color:#333;">Attraction Rate</a>
-                </td></tr>
-            <tr><td>
-                    <a tabindex="0" class="glyphicon glyphicon-question-sign pull-right popover_styles" role="button" data-toggle="popover"
                        data-content="Search by loot name to see the location and drop rate of a specific loot."></a>
                     <a href="/loot.php" style="display:block;text-decoration:none;color:#333;">Looter</a>
                 </td></tr>
