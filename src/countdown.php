ff08f089134db0e8ed1c3283b6a7502cb1b382e3
Jack <devjacksmith@gmail.com>
2020-12-07 16:15:07 -0700

    Added hidden settings file for easier config

--- src/countdown.php
+++ src/countdown.php
@@ -3,9 +3,11 @@
 //    $css = "styles/maphelper.css";
     $js = "scripts/countdown.js";
     require_once "new-common-header.php";
+    require_once "countdown-settings.php";
 
-    echo '<h1 id="eventname"></h1>';
+    echo '<h1 id="eventname">' . $eventName . '</h1>';
     echo '<h3 id="countdown"></h3>';
+    echo '<input id="countdownDate" type="hidden" value="' . $countdownDate . '">';
 
     require_once "new-common-footer.php";
 ?>
