6d849a26c935fd8c0bda7d22d2b558c84004f215
Jack <devjacksmith@gmail.com>
2020-12-16 18:21:19 +0000

    Added cutesy time increments - hunts, golems

--- src/scripts/countdown.js
+++ src/scripts/countdown.js
@@ -17,10 +17,12 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var horns = Math.floor(distance / (1000 * 60 * 15));
+  var golems = Math.floor(distance / (1000 * 60 * 15 * 25));
 
   // Display the result in the element with id="demo"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  + minutes + "m " + seconds + "s (" + horns + " horns, " + golems + " unbelled golem cycles)";
 
   // If the count down is finished, write some text
   if (distance < 0) {
