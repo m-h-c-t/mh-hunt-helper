2e4ba4c26cc862f2176aa37f807f2dcaf802f833
Jack <devjacksmith@gmail.com>
2020-12-16 16:01:38 +0000

    Countdown to empty lot; changed display of countdown

--- src/countdown.php
+++ src/countdown.php
@@ -8,6 +8,7 @@
     echo '<h1 id="eventname">' . $eventName . '</h1>';
     echo '<h3 id="countdown"></h3>';
     echo '<input id="countdownDate" type="hidden" value="' . $countdownDate . '">';
+    echo '<h4 id="countdownDisplayDate">That is ' . $countdownDate . ' UTC</h3>';
     echo '<br/>Special thanks to <a href="https://github.com/chirpphixel/chirpphixel.github.io" target="_blank">chirpphixel</a>
     and <a href="https://sites.google.com/site/mhtimerslinks/" target="_blank">Chad\'s Timers page</a> for inspiration.<br/>';
 
