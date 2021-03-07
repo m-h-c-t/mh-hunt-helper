6b144077e4ef5599b7fb8dc2de0d02396c9cb2ed
Jack <devjacksmith@gmail.com>
2021-03-07 13:04:24 -0700

    switched to new header and footer in tracker

--- src/tracker.php
+++ src/tracker.php
@@ -1,15 +1,10 @@
-<html lang="en">
-<head>
-    <title>MHCT Tracker</title>
-    <?php require "common_head.php"; ?>
-</head>
-<body style="text-align: center;" class="text-center">
-    <!-- Jumbotron -->
-    <div class="jumbotron">
-        <h1>MHCT Tracker</h1>
-        <a href="/" class="clickable"><span class="glyphicon glyphicon-chevron-left"></span> MHCT Tools</a>
-    </div>
-    <div class="container">
+<?php
+    $title = "MHCT Tracker";
+    // $css = "styles/loot.css";
+    // $js = "scripts/attraction.js";
+    require_once "new-common-header.php";
+?>
+
         <?php
         $file_name = 'tracker.json';
         $location = 'Unknown';
@@ -37,11 +32,8 @@
             <small>Last seen (after last move): <?php print $time_since; ?> ago.</small>
         </h3><br/><br/>
 
-        <?php require_once "stats.php"; ?><br/><br/>
+        <?php require_once "stats.php"; ?><br/>
         <?php //require_once "missing_mice.php"; ?>
 
-        <br/><p class="text-center">For more info, copy of the data, or if you want to help with data gathering, please look <a href="/">here</a>.</p>
-    </div>
-    <?php require "common_footer.php"; ?>
-</body>
-</html>
+
+<?php require_once "new-common-footer.php"; ?>
