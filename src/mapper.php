2a3720d54390e67ed777483e7c9a8fbcedfdac72
Jack <devjacksmith@gmail.com>
2021-03-07 12:57:16 -0700

    switched to new header and footer in mapper

--- src/mapper.php
+++ src/mapper.php
@@ -1,38 +1,24 @@
-<html lang="en">
-<head>
-    <title>MHCT Mapper</title>
-    <?php require "common_head.php"; ?>
-    <script defer src="/scripts/mapper.js"></script>
-</head>
-<body style="text-align: center;" class="text-center">
-    <!-- Jumbotron -->
-    <div class="jumbotron">
-        <h1>MHCT Mapper</h1>
-        <a href="/" class="clickable"><span class="glyphicon glyphicon-chevron-left"></span> MHCT Tools</a>
-    </div>
-    <div class="container">
+<?php
+    $title = "MHCT Mapper";
+    // $css = "styles/loot.css";
+    $js = "scripts/mapper.js";
+    require_once "new-common-header.php";
+?>
+
         <input id="item_type" type="hidden" value="loot">
         <?php
-
         if (!empty($_GET['item'])) {
             print '<input id="prev_item" type="hidden" value="' . $_GET['item'] . '">';
-        }
+        } ?>
 
-        print '
-            <div class="input-group col-sm-6 col-sm-offset-3">
-                <div class="input-group-addon"><strong>Map:</strong></div>
-                <input name="item" id="item" class="form-control input-lg" type="text" placeholder="Start typing map name and select." autofocus>
-                <div id="erase_item" class="input-group-addon fakebutton"><span class="glyphicon glyphicon-remove"></span></div>
-            </div>
+        <div class="input-group col-sm-6 col-sm-offset-3">
+            <div class="input-group-addon"><strong>Map:</strong></div>
+            <input name="item" id="item" class="form-control input-lg" type="text" placeholder="Start typing map name and select." autofocus>
+            <div id="erase_item" class="input-group-addon fakebutton"><span class="glyphicon glyphicon-remove"></span></div>
+        </div>
         <br/>';
 
-        //Ajax here
-        print '<div id="results_total"></div>';
-        print '<div id="results" class="table-responsive"></div>';
-
-        print '<br/><p class="text-center">For more info, copy of the data, or if you want to help with data gathering, please look <a href="https://www.agiletravels.com">here</a>.</p>';
-        ?>
+        <div id="results_total"></div>
+        <div id="results" class="table-responsive"></div>
     </div>
-    <?php require "common_footer.php"; ?>
-</body>
-</html>
+<?php require_once "new-common-footer.php"; ?>
