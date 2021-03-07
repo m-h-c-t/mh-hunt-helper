4a26728c673edaac406ac118c7ccdb3ce5fd7f85
Jack <devjacksmith@gmail.com>
2021-03-07 12:00:53 -0700

    added sort by starts with to converter

--- src/scripts/converter.js
+++ src/scripts/converter.js
@@ -50,6 +50,11 @@ $( function() {
         $('#item').autocomplete({
             source: function(request, response) {
                 var results = $.ui.autocomplete.filter(items, request.term);
+
+                results.sort(function(a, b) {
+                    return a.value.toUpperCase().indexOf(request.term.toUpperCase()) - b.value.toUpperCase().indexOf(request.term.toUpperCase());
+                });
+
                 response(results.slice(0, 10));
             },
             delay: 0,
