e8592ee6b85677c70fb739adece1d65b913f4cbf
Jack <devjacksmith@gmail.com>
2021-09-05 19:01:47 +0000

    Added rh tracking in db

--- src/rh-intake.php
+++ src/rh-intake.php
@@ -2,18 +2,30 @@
 
 require_once "check-direct-access.php";
 
-// Relic Hunter
+// Check if Relic Hunter submission
 if (!empty($_POST['rh_environment']) && !empty($_POST['user_id'])) {
     recordRelicHunter();
 	sendResponse('success', "Thanks for reporting RH!");
     // Dies in sendResponse
 }
+// else it's not an RH submission
 
 function recordRelicHunter() {
     if (empty($_POST['entry_timestamp']) || !is_numeric($_POST['entry_timestamp'])) {
-        return;
+        error_log("RH Submission bad or missing timestamp.");
+        die();
     }
+    recordRelicHunterInFile();
+    recordRelicHunterInDB();
+}
 
+function recordRelicHunterInDB() {
+    global $pdo;
+    $query = $pdo->prepare('INSERT IGNORE INTO rh_tracker(`date`, location_id) SELECT ?, l.id FROM locations l WHERE l.name LIKE ?;');
+    $query->execute(array(date("Y-m-d"), $_POST['rh_environment']));
+}
+
+function recordRelicHunterInFile() {
     $file_name = 'tracker.json';
     $location = getAliasLocationName(filter_var($_POST['rh_environment'], FILTER_SANITIZE_STRING));
 
