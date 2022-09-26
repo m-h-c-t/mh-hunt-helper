abd8674236e34c6dcbdb54595f7df53cf69002ea
Jack <DevJackSmith@users.noreply.github.com>
2022-09-26 14:29:28 +0000

    Adding message for testers

--- src/check-version.php
+++ src/check-version.php
@@ -2,6 +2,10 @@
 
 require_once "check-direct-access.php";
 
+if ($_POST['extension_version'] == '0') {
+    sendResponse('success', "Thanks for TESTING!");
+}
+
 // Extension version check
 if (!in_array($_POST['extension_version'], $allowed_extension_versions)) {
     //    error_log("User $_POST[user_id]: Bad version: " . $_POST['extension_version'] . ip_display());
