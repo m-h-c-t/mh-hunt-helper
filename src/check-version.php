f6ff1afa043c4fd3d40625322ae5df64749cc7bb
Jack <DevJackSmith@users.noreply.github.com>
2022-11-13 17:41:27 -0800

    Removing config param

--- src/check-version.php
+++ src/check-version.php
@@ -3,11 +3,6 @@
 require_once "check-direct-access.php";
 require_once "config.php";
 
-// Nice response from prod server
-// if ($_POST['extension_version'] == '0') {
-    // sendResponse('success', "Thanks for TESTING extension! You are pointing to prod server.");
-// }
-
 // Extension version check
 if (!in_array($_POST['extension_version'], $allowed_extension_versions)) {
     error_log("User " . $_POST['hunter_id_hash'] . ": Bad version: " . $_POST['extension_version']);
