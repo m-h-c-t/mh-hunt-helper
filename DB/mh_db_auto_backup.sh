8c6f02508c4f9872088423cba66ee3e49d67b93d
Rob Wilson <aardwolf98@gmail.com>
2023-03-06 18:16:20 -0500

    Added a chmod

--- DB/mh_db_auto_backup.sh
+++ DB/mh_db_auto_backup.sh
@@ -74,6 +74,7 @@ echo "===== Copying to keybase ====="
 
 su user -c 'cp converter_weekly.sql.gz converter_weekly.txt.zip mapspotter_weekly.sql.gz mapspotter_weekly.txt.zip hunthelper_weekly.sql.gz hunthelper_weekly.txt.zip last_updated.txt  /keybase/public/devjacksmith/mh_backups/weekly/'
 su user -c 'cp converter_weekly.sql.gz converter_weekly.txt.zip mapspotter_weekly.sql.gz mapspotter_weekly.txt.zip hunthelper_weekly.sql.gz hunthelper_weekly.txt.zip last_updated.txt  /backups/mh_backups/weekly/'
+su user -c 'chmod og+r /backups/mh_backups/weekly/*'
 
 rm -rf *.sql.gz *.txt.zip
 
