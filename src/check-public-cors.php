04ac13b7728f018e9cb1ff23f2204eb876b401ef
Jack <DevJackSmith@users.noreply.github.com>
2023-03-22 23:21:51 -0600

    and a typo

--- src/check-public-cors.php
+++ src/check-public-cors.php
@@ -7,7 +7,7 @@ $public_cors_whitelist = [
     'https://dev.markethunt.win'
 ];
 
-if (array_key_exists('HTTP_ORIGIN', $_SERVER) {
+if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     if (!in_array($_SERVER['HTTP_ORIGIN'], $public_cors_whitelist)) {
         error_log("Origin didn't match, requests origin was: " . $_SERVER['HTTP_ORIGIN']);
         die();
