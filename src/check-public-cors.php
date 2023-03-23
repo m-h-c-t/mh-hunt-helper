8378d34250fbef7486cd68af4b6d5c5cc252a4de
Jack <DevJackSmith@users.noreply.github.com>
2023-03-22 23:19:20 -0600

    fixing filters cors

--- src/check-public-cors.php
+++ src/check-public-cors.php
@@ -7,12 +7,14 @@ $public_cors_whitelist = [
     'https://dev.markethunt.win'
 ];
 
-if ($_SERVER['HTTP_ORIGIN'] && !in_array($_SERVER['HTTP_ORIGIN'], $public_cors_whitelist)) {
-    error_log("Origin didn't match, requests origin was: " . $_SERVER['HTTP_ORIGIN']);
-    die();
+if ($_SERVER['HTTP_ORIGIN']) {
+    if (!in_array($_SERVER['HTTP_ORIGIN'], $public_cors_whitelist)) {
+        error_log("Origin didn't match, requests origin was: " . $_SERVER['HTTP_ORIGIN']);
+        die();
+    } else {
+        header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    }
 }
-
-header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     die();
