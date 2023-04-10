0456d07e501d9f2a959118ae0b341dfd9efce2f4
Jack <DevJackSmith@users.noreply.github.com>
2023-04-10 11:25:21 -0600

    removing file check

--- src/locations/locations.php
+++ src/locations/locations.php
@@ -6,6 +6,6 @@ $location_id_to_filter_name = [
 ];
 
 $loc_id = filter_var($_POST['location']['id'], FILTER_VALIDATE_INT);
-if (is_numeric($loc_id) && array_key_exists($loc_id, $location_id_to_filter_name) && file_exists("locations/" . $location_id_to_filter_name[$loc_id] . ".php")) {
+if (is_numeric($loc_id) && array_key_exists($loc_id, $location_id_to_filter_name)) {
     include_once "locations/" . $location_id_to_filter_name[$loc_id] . ".php";
 }
